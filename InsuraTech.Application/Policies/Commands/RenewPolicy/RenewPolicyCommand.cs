ae5df6c0f4682a4b5fd5703ffdea5337eace7412
Daniel <daniel.ramirez@sofka.com.co>
2026-03-16 16:15:47 -0500

    added_renewal_commands_Policy_handler

---
+++ InsuraTech.Application/Policies/Commands/RenewPolicy/RenewPolicyCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsuraTech.Application.Policies.DTOs;
+using MediatR;
+
+namespace InsuraTech.Application.Policies.Commands.RenewPolicy
+{
+    public sealed class RenewPolicyCommand : IRequest<PolicyResponse>
+    {
+        public Guid PolicyId { get; init; }
+        public DateOnly NewCoverageStartDate { get; init; }
+        public DateOnly NewCoverageEndDate { get; init; }
+    }
+}
